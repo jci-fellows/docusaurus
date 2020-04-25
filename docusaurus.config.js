5e99c70e9f759e2fe0af10935cdd60c894baffd7
Philip Johnson <johnson@hawaii.edu>
2020-04-25 10:27:44 -1000

    fix the edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/jci-fellows/docusaurus/',
+            'https://github.com/jci-fellows/docusaurus/edit/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
